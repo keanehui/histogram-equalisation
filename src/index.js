1053b6c49dd94a8158f94bc5745fec572bd5af70
ATKTC <ktcwkwan@connect.ust.hk>
2023-05-05 13:54:22 +0800

    firebase setting push

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBf1dGPTnsVmzSHttx0ulkWBjpehb8ONH8",
+  authDomain: "histrogram-equalization.firebaseapp.com",
+  projectId: "histrogram-equalization",
+  storageBucket: "histrogram-equalization.appspot.com",
+  messagingSenderId: "224612019694",
+  appId: "1:224612019694:web:be91894695935f023632ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
