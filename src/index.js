17a9d6e750ebd4b635876e2c701815858f0a21e4
keanehui <kawonghui@gmail.com>
2023-05-05 14:08:44 +0800

    fix issues of firebase deploy

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
